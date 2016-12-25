0d55138beb389000608319cdca19f87dca96deb7
Kevin Fodness <kevin@kevinfodness.com>
2016-12-25 15:55:50 -0500

    Added timer to increment time every second.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import tkinter, time
-l = tkinter.Label(text = time.strftime('%A\n%B %d %Y\n%I:%M:%S %p'))
-l.pack()
-l.mainloop()
+import tkinter as tk
+import time
+
+def Draw():
+    global timeText
+    frame = tk.Frame(root, width=100, height=100, relief='solid', bd=1)
+    frame.place(x=10, y=10)
+    timeText = tk.Label(frame, text=time.strftime('%A\n%B %d %Y\n%I:%M:%S %p'))
+    timeText.pack()
+
+def Refresher():
+    global timeText
+    timeText.configure(text=time.strftime('%A\n%B %d %Y\n%I:%M:%S %p'))
+    timeText.after(1000, Refresher)
+
+root = tk.Tk()
+Draw()
+Refresher()
+root.mainloop()
